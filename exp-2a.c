c62b994ece36aaf848c76bc4cfd0e9379d7cfac5
shanawzkhan <khanshanawaz@eng.rizvi.edu.in>
2025-10-06 16:54:50 +0530

    Create exp-2a.c

---
+++ exp-2a.c
@@ -0,0 +1,22 @@
+/*name:shanawaz
+UIN:251C039
+CLASS:CIVIL
+DIV:B*/
+#include<stdio.h> 
+int main() {
+    int num;
+    printf("enter a number= ");
+    scanf("%d",&num);
+    if(num%2 == 0 ) {
+        //code if condition is true
+        printf("number is even ");
+
+    } else {
+        //code if condition if false
+        printf("number is odd or zero ");
+    }
+    return 0;
+}
+   
+  
+   
