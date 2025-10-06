1cf745d0be773ab5364639837c2eb63875cf989e
shanawzkhan <khanshanawaz@eng.rizvi.edu.in>
2025-10-06 16:40:49 +0530

    Create exp-1b.c

---
+++ exp-1b.c
@@ -0,0 +1,21 @@
+/*name:shanawaz
+UIN:251C039
+CLASS:CIVIL
+DIV:B*/
+#include<stdio.h> 
+int main() {
+    float phy, chem, math, avg;
+    printf("enter physics marks: ");
+    scanf("%f, & phy");
+    printf("enter chemistry marks: ");
+    scanf("%f", &chem);
+    printf("enter math marks: ");
+    scanf("%f", &math);
+    avg =(phu + chem + math) / 3;
+    printf("average pcm marks: %.2f%%\n", avg);
+    (aavg >= 50) ?printf("student is eligible for addmissio\n") :printf("student is not eligible for admission.\n");
+    return 0;
+}
+   
+  
+   
