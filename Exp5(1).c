d7ab2478b504fc2fe4b4bf65493fd2e492554289
shanawzkhan <khanshanawaz@eng.rizvi.edu.in>
2025-11-30 21:45:11 +0530

    Exp5(1).c

---
+++ Exp5(1).c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int factorialIterative(int num) 
+{
+    int prod=1;
+    for(int i=1; i<=num; i++)
+    {
+        prod*=i; 
+    }
+    return prod;
+}
+int main()
+{
+    int num;
+    printf("\t\t\t**Iterative Factorial Function**\n\n"); 
+    printf("Enter Number = ");
+    scanf("%d", &num);
+    printf("Factorial of %d is %d",num,factorialIterative(num)); 
+    return 0;
+}
