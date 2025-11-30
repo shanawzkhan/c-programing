df83f614abe4b19ee933f45c8875d9ba1dfff5f5
shanawzkhan <khanshanawaz@eng.rizvi.edu.in>
2025-11-30 21:52:51 +0530

    Exp8(1).c

---
+++ Exp8(1).c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, length = 0;
+
+    printf("Enter a string: ");
+    fgets(str, 100, stdin); 
+
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == '\n') {   
+            str[i] = '\0';      
+            break;              
+        }
+        length++;
+    }
+
+    printf("Length of the string = %d\n", length);
+
+    return 0;
+}
