6f560049a203c74b3fa8d26e529730b49813d7e7
shanawzkhan <khanshanawaz@eng.rizvi.edu.in>
2025-11-30 21:54:19 +0530

    Exp8(2).c

---
+++ Exp8(2).c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main() {
+    char str[100];
+    int i, length = 0, flag = 0;
+    
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == '\n') {
+            str[i] = '\0';
+            break;
+        }
+        length++;
+    }
+    
+    for (i = 0; i < length / 2; i++) {
+        if (str[i] != str[length - 1 - i]) {
+            flag = 1;
+            break;
+        }
+    }
+    
+    if (flag == 0)
+        printf("The string is a palindrome.\n");
+    else
+        printf("The string is not a palindrome.\n");
+    
+    return 0;
+}
